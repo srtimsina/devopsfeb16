d16a38ad83f85cd7d7a8d0875b881d14d5ec9797
Suryaraj Timsina <devopsuryaraj@gmail.com>
2025-03-17 02:13:28 +0000

    install docker pkgs

--- install_apache.sh
+++ install_apache.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "Installing apache2 pkg"
 sudo apt update
-sudo apt install apache2 -y
+sudo apt install apache2 curl wget -y
 if [ -f /var/run/apache2/apache2.pid ];
 then
   echo "apache2 is running"
@@ -10,3 +10,24 @@ else
  sudo systemctl start apache2
 sudo systemctl enable apache2
 fi
+
+echo "Install docker packages..."
+for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc; do sudo apt-get remove $pkg; done
+
+# Add Docker's official GPG key:
+sudo apt-get update
+sudo apt-get install ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+# Add the repository to Apt sources:
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+sudo docker run hello-world
